8ed1c18d6f84b3fd6db0093573dff3b97aaf27ff
vjoki <vjoki@zv.fi>
2020-12-09 21:05:18 +0200

    Add NShotKWayDataset

--- snn/librispeech/data_loader.py
+++ snn/librispeech/data_loader.py
@@ -1,9 +1,9 @@
 import os
 import random
 import collections
-from typing import Optional, List, Dict, Tuple
+import itertools
+from typing import Optional, List, Dict, Tuple, Set
 from typing_extensions import Final
-import numpy as np
 import torch
 import torch.nn.functional as F
 import torchaudio
@@ -253,7 +253,135 @@ class TripletDataset(Dataset):
         waveform1 = process_waveform(waveform1, max_frames_per_sample=max_frames)
         waveform2 = process_waveform(waveform2, max_frames_per_sample=max_frames)
         waveform3 = process_waveform(waveform3, max_frames_per_sample=max_frames)
-        assert (max_frames is None
-                or waveform1.size(0) == waveform2.size(0) == waveform3.size(0) == max_frames)
+        assert (max_frames is None or waveform1.size(0) == waveform2.size(0) == waveform3.size(0) == max_frames)
 
         return (waveform1, waveform2, waveform3)
+
+
+class NShotKWayDataset(Dataset):
+    def __init__(self, dataset: dset.LIBRISPEECH,
+                 num_shots: int = 1,
+                 num_ways: int = 5,
+                 rir_path: str = './data/RIRS_NOISES/',
+                 augment: bool = True,
+                 random: bool = False,
+                 max_sample_length: Optional[int] = None):
+        super().__init__()
+        assert num_shots > 0
+        assert num_ways > 0
+
+        self.n_shots: Final = num_shots
+        self.n_ways: Final = num_ways
+        self.dataset: Final = dataset
+        self._augment: Final = augment
+        self._max_length: Final = max_sample_length
+        self._transform: Final = Compose([
+            AddGaussianSNR(min_SNR=0.5, max_SNR=0.5, p=1.0),
+            #AddImpulseResponse(os.path.join(rir_path, 'real_rirs_isotropic_noises'), p=0.5),
+            #AddShortNoises(os.path.join(rir_path, 'pointsource_noises'), p=0.5)
+        ])
+
+        speaker_set = set([])
+        samples: Dict[str, List[int]] = collections.defaultdict(list)
+        for i, fileid in enumerate(self.dataset._walker):
+            (speaker_id, _, _) = fileid.split("-")
+            samples[speaker_id].append(i)
+            speaker_set.add(speaker_id)
+        speakers = list(speaker_set)
+
+        self.entries = []
+
+        # Choose 1 positive supports (e.g. where speaker = query speaker).
+        positive = 1
+        negative = self.n_ways - positive
+
+        while len(speakers) >= self.n_ways:
+            support_sets: List[List[int]] = []
+
+            c = 0
+            speaker_id = speakers[c]
+            while len(samples[speaker_id]) < self.n_shots * positive + 1:
+                c += 1
+                speaker_id = speakers[c]
+
+            query = samples[speaker_id].pop()
+
+            # Add n-shots from query speaker to the support set.
+            pos_support: List[List[int]] = []
+            for _ in range(0, positive):
+                pos_support.append([samples[speaker_id].pop() for _ in range(0, self.n_shots)])
+                if len(samples[speaker_id]) <= 0:
+                    speakers.remove(speaker_id)
+            support_sets.extend(pos_support)
+
+            # Choose speakers where speaker != query speaker.
+            neg_support: List[List[int]] = []
+            neg_speakers = []
+            remove_speakers = []
+            for sid in speakers:
+                if sid == speaker_id:
+                    continue
+                if len(samples[sid]) < self.n_shots:
+                    remove_speakers.append(sid)
+                    continue
+                neg_speakers.append(sid)
+                if len(neg_speakers) >= negative:
+                    break
+
+            # Break if we ran out of speakers to use for the support set.
+            if len(neg_speakers) < negative:
+                break
+
+            # Add n-shots for the non-query speaker ways.
+            for sid in neg_speakers:
+                neg_support.append([samples[sid].pop() for _ in range(0, self.n_shots)])
+                if len(samples[sid]) <= 0:
+                    remove_speakers.append(sid)
+            support_sets.extend(neg_support)
+
+            for sid in remove_speakers:
+                speakers.remove(sid)
+
+            assert len(support_sets) == self.n_ways
+            self.entries.append((query, support_sets))
+
+        # assert len(self.entries) == (len(self.dataset) // (self.n_ways * self.n_shots + 1))
+        print("Created {}-shot {}-way dataset of {} tasks from {} samples."
+              .format(self.n_shots, self.n_ways, len(self.entries), len(self.dataset)))
+
+    def __len__(self):
+        # return len(self.dataset) // (self.n_ways * self.shot + 1)
+        return len(self.entries)
+
+    def __getitem__(self, index):
+        qi, support_indices = self.entries[index]
+
+        (wf, sample_rate, _, query_speaker, _, _) = self.dataset.__getitem__(qi)
+        max_frames = self._max_length * sample_rate if self._max_length else None
+        wf = process_waveform(wf, max_frames_per_sample=max_frames)
+        query = wf
+
+        support_set = []
+        labels = []
+
+        assert len(support_indices) == self.n_ways
+        for way in support_indices:
+            shots = []
+            assert len(way) == self.n_shots
+            for shot in way:
+                (wf, _, _, speaker, _, _) = self.dataset.__getitem__(shot)
+
+                if self._augment:
+                    assert wf.size(0) == 1
+                    wf = wf.squeeze()
+                    wf = torch.from_numpy(self._transform(wf.t().numpy(), sample_rate=sample_rate))
+                    if torch.isnan(wf).any() or torch.isinf(wf).any():
+                        print('invalid input detected at augment', wf)
+
+
+                wf = process_waveform(wf, max_frames_per_sample=max_frames)
+                shots.append(wf)
+            labels.append(torch.LongTensor([1] if query_speaker == speaker else [0]))
+            support_set.append(shots)
+
+        return (query, support_set, torch.cat(labels))
