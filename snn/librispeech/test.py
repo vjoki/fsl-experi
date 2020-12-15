a71b50cb184cbf4603f08c56db542be4c4789849
vjoki <vjoki@zv.fi>
2020-12-15 03:23:28 +0200

    librispeech.test: Allow specifying test dataset from a list

--- snn/librispeech/test.py
+++ snn/librispeech/test.py
@@ -1,15 +1,19 @@
 from argparse import ArgumentParser
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.data import DataLoader
 
 from snn.librispeech.model import TwinNet
-
+from snn.librispeech.data_loader import PairDatasetFromList
 
 def test():
     parser = ArgumentParser()
     general = parser.add_argument_group('General')
     general.add_argument('--log_dir', type=str, default='./lightning_logs/', help='Tensorboard log directory')
     general.add_argument('--model_path', type=str, required=True)
+    general.add_argument('--test_list', type=str, default=None, help='File containing list of test pairs and labels')
+    general.add_argument('--test_path', type=str, default='./',
+                         help='Test data path, only applies when using --test_list.')
     parser = TwinNet.add_model_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args()
@@ -29,7 +33,16 @@ def test():
     model.eval()
     model.freeze()
 
-    trainer.test(model=model, ckpt_path=args.model_path)
+    test_dataloader = None
+    if args.test_list:
+        test_dataloader = DataLoader(
+            PairDatasetFromList(args.test_list, args.test_path,
+                                max_sample_length=args.max_sample_length),
+            batch_size=args.batch_size, shuffle=False,
+            num_workers=args.num_workers, pin_memory=True
+        )
+
+    trainer.test(model=model, ckpt_path=args.model_path, test_dataloaders=test_dataloader)
 
 
 if __name__ == "__main__":
