6b0697b60aca5118c380fdfd5c8c7f0b09e14c95
vjoki <vjoki@zv.fi>
2021-01-29 18:52:10 +0200

    Remove F.normalize from SoftmaxLoss

--- snn/librispeech/loss/softmax.py
+++ snn/librispeech/loss/softmax.py
@@ -14,7 +14,6 @@ class SoftmaxLoss(nn.Module):
     def forward(self, x, label=None):
         # TODO: Not sure what the rationale for N -> nClasses is...
         # B*W*SxN -> B*W*SxnClasses
-        x = F.normalize(x, p=2, dim=1)
         x = self.fc(x)
         nloss = self.criterion(x, label)
         prec1 = accuracy(x.detach(), label.detach(), topk=(1,))[0]
